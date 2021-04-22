2dc452817d9dbfdb188e893fd6d1f126ae7db340
Regan Wheeless <regan.wheeless@gmail.com>
2021-04-22 00:13:01 -0400

    basics
    character movements, camera linked to player

---
+++ Dementium/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1;
+    //public float jump = 1;
+
+    Rigidbody2D rigidbody2d;
+    
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+    }
+}
